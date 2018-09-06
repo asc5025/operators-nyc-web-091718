e13db190938676f82e3f7a720f7cdb2ce12d7249
Albert Chun <albert.s.chun@gmail.com>
2018-09-06 18:33:51 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-  not_safe? ? speed > 60 || speed < 40 : speed <= 60 && speed >= 40
+  not_safe?(speed) ? speed > 60 || speed < 40 : speed <= 60 && speed >= 40
 end
 
 
